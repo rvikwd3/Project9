5fa36450a0af3a43cd6edf759dc790c7c36f5e5e
rvikwd3 <rvikwd3@outlook.com>
2018-10-26 13:48:54 +0530

    Loading & Saving images tutroial

--- DevilTuts/HelloWorld.cpp
+++ DevilTuts/HelloWorld.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 
-// Try includeing the libraries
+// Try including the libraries
 #include <IL/il.h>
+#include <IL/ilu.h>
+#include <IL/ilut.h>
 
 int main(){
-	std::cout << "Hello DevIL world!" << std::endl;;
+	// Include IL
+	ilInit();
+	std::cout << "Initialized IL" << std::endl;;
+
+	// Include ILU
+	//iluInit();
+
+	// Include ILUT with OpenGL API support
+	//ilutRenderer(ILUT_OPENGL);
+	
+	// Generate an Image Name
+	ILuint image_name;
+	ilGenImages(1, &image_name);
+
+	// Bind image_name
+	ilBindImage(image_name);
+	std::cout << "Bound image_name" << std::endl;;
+
+	// Load the 2x2 image
+	char filename[] = "2x2.png";
+	ilLoadImage(filename);
+	std::cout << "Loaded 2x2.png" << std::endl;;
+
+	// Save the image under a different name
+	ilSaveImage("convert.png");
+	std::cout << "Saved convert.png" << std::endl;;
+
+	// Delete the image
+	ilDeleteImage(image_name);
+
+	std::cout << "" << std::endl;;
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Project9
+
+DevIL Usage:
+
+Use ```-lIL``` for DevIL library linking
+The other library flags are ```-lILU, -lILUT``` respectively
+
+Current todo:
+*	Import image file
+*	Convert image to pixel matrix
+*	Convert matrix to HSV matrix
